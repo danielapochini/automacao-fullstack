e4cb013a67917a60d8ec87c148d02922d6962d24
danielapochini <daniela.pochini@fatec.sp.gov.br>
2020-02-05 16:20:53 -0300

    Testando API Post

--- nflix-api/spec/factories.rb
+++ nflix-api/spec/factories.rb
@@ -12,9 +12,39 @@ FactoryBot.define do
         end
     end
 
+    factory :registered_user, class: UserModel do
+        full_name {"Daniela Pereira"}
+        email {"eu@papito.io"}
+        password {"teste1234"}
+
+        after(:build) do |user|
+            #puts "apagando o email " + user.email
+            Database.new.delete_user(user.email)
+            ApiUser.save(user.to_hash)
+        end
+    end
+
     factory :user_wrong_email, class: UserModel do
         full_name {"Daniela Pochini"}
         email {"qaninja#papito.io"}
         password {"teste1234"}
+    end
+
+    factory :empty_name_user, class: UserModel do
+        full_name {""}
+        email {"qaninja@papito.io"}
+        password {"teste1234"}   
+    end
+
+    factory :empty_email_user, class: UserModel do
+        full_name {"Daniela Pochini"}
+        email {""}
+        password {"teste1234"}   
+    end
+
+    factory :empty_password_user, class: UserModel do
+        full_name {"Daniela Pochini"}
+        email {"qaninja@papito.io"}
+        password {""}   
     end
 end
--- nflix-api/spec/post_spec.rb
+++ nflix-api/spec/post_spec.rb
@@ -2,10 +2,18 @@ describe "post" do
     context "when new user" do
         before do
             @new_user = build(:user).to_hash
-            
             @result = ApiUser.save(@new_user)
         end
         it {expect(@result.response.code).to eql "200"}
+    end
+
+    context "when duplicate email" do
+        before do
+            @new_user = build(:registered_user).to_hash
+            @result = ApiUser.save(@new_user)
+        end      
+        it {expect(@result.response.code).to eql "409"}
+        it {expect(@result.parsed_response["msg"]).to eql "Oops. Looks like you already have an account with this email address."}
     end
     
     context "when wrong email" do
@@ -15,5 +23,32 @@ describe "post" do
         end
         it {expect(@result.response.code).to eql "412"}
         it {expect(@result.parsed_response["msg"]).to eql "Oops. You entered a wrong email."}
+    end
+
+    context "when empty name" do
+        before do
+            @new_user = build(:empty_name_user).to_hash
+            @result = ApiUser.save(@new_user)
+        end
+        it {expect(@result.response.code).to eql "412"}
+        it {expect(@result.parsed_response["msg"]).to eql "Validation notEmpty on full_name failed"}
+    end
+
+    context "when empty email" do
+        before do
+            @new_user = build(:empty_email_user).to_hash
+            @result = ApiUser.save(@new_user)
+        end
+        it {expect(@result.response.code).to eql "412"}
+        it {expect(@result.parsed_response["msg"]).to eql "Validation notEmpty on email failed"}
+    end
+
+    context "when empty password" do
+        before do
+            @new_user = build(:empty_password_user).to_hash
+            @result = ApiUser.save(@new_user)
+        end
+        it {expect(@result.response.code).to eql "412"}
+        it {expect(@result.parsed_response["msg"]).to eql "Validation notEmpty on password failed"}
     end
 end
